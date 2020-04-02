4b163509be364519a5e513203adbfc27476d50b4
jingjielim <jingjie3000@gmail.com>
2020-04-02 19:38:08 -0400

    Edit seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = { email: 'a@a', password: '1', password_confirmation: '1' }
+user = { email: 'shopper@shopping.com', password: 'shopee', password_confirmation: 'shopee' }
 User.create(user)
 user1 = User.find(1)
 
@@ -28,20 +28,40 @@ stores.each do |store|
   new_store.save
 end
 
-puts 'Stores created'
+const num_stores = Store.all.length
+puts "#{num_stores} stores created"
 
 items = [
   { name: 'eggs', unit: 'dozen' },
   { name: 'chicken wings', unit: 'lbs' },
   { name: 'chicken thighs', unit: 'lbs' },
-  { name: 'mushrooms', unit: 'lbs' },
+  { name: 'chicken breasts', unit: 'lbs' },
+  { name: 'white button mushrooms', unit: 'pkt' },
+  { name: 'enoki mushrooms', unit: 'lbs' },
   { name: 'ham', unit: 'lbs' },
-  { name: 'salmon', unit: 'lbs' },
-  { name: 'potato', unit: 'lbs' },
+  { name: 'salmon fillet', unit: 'lbs' },
+  { name: 'salmon skin', unit: 'lbs' },
+  { name: 'salmon bones', unit: 'lbs' },
+  { name: 'broccoli', unit: 'lbs' },
+  { name: 'cabbage', unit: 'lbs' },
+  { name: 'lettuce', unit: 'lbs' },
+  { name: 'onions', unit: 'lbs' },
+  { name: 'bread', unit: 'loaf' },
+  { name: 'cheese', unit: 'pkt' },
+  { name: 'apples', unit: 'lbs' },
+  { name: 'pears', unit: 'lbs' },
+  { name: 'watermelon', unit: 'lbs' },
+  { name: 'carrots', unit: 'lbs' },
+  { name: 'peas', unit: 'lbs' },
+  { name: 'corn', unit: 'lbs' },
+  { name: 'pears', unit: 'lbs' },
+  { name: 'sweet onions', unit: 'lbs' },
+  { name: 'potatoes', unit: 'lbs' },
   { name: 'crabs', unit: 'lbs' },
   { name: 'prawns', unit: 'lbs' },
   { name: 'ice cream', unit: 'pint' },
-  { name: 'milk', unit: 'gal' }
+  { name: 'milk', unit: 'gal' },
+  { name: 'toilet paper', unit: 'roll' }
 ]
 
 items.each do |item|
@@ -51,9 +71,10 @@ items.each do |item|
   new_item.save
 end
 
-puts 'Items created'
+const num_items = Item.all.length
+puts "#{num_items} items created"
 
-1.upto(100) {
+1.upto(200) {
   Random.new_seed
   new_rand = Random.new
   rand_store = new_rand.rand(stores.length + 1)
@@ -63,4 +84,5 @@ puts 'Items created'
   new_price.save
 }
 
-puts 'Prices created'
+const num_prices = Price.all.length
+puts "#{num_prices} prices created"
